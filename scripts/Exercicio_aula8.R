dd09bd03081db622cc2aa78a3d69b25e19ac9c1c
Helena Santos <helenafsantos8@gmail.com>
2021-05-24 14:18:58 -0300

    inicio estruturação

--- scripts/Exercicio_aula8.R
+++ scripts/Exercicio_aula8.R
@@ -10,6 +10,7 @@ library(tidyverse)
 
 #Dataset
 library(help = "datasets")
+library(help = "datasets") #Datasets
 
 #swiss: Swiss Fertility and Socioeconomic Indicators (1888) Data - Standardized fertility measure and socio-economic indicators for each of 47 French-speaking provinces of Switzerland at about 1888. 
 
@@ -19,7 +20,21 @@ freq(swiss) # frequência das variáveis fator
 plot_num(swiss) # exploração das variáveis numéricas
 profiling_num(swiss) # estatísticas das variáveis numéricas
 
+
+
 #Estruturação 
+# Compartilhe com a gente um código em que você implementa um pivô long to wide ou wide to long.
+
+
+library(data.table)
+library(dplyr)
+library(poliscidata)
+
+glimpse(states)
+
+poliscidata::states
+
+base_states <- states %>% pivot_wider(names_from = )
 
 #Limpeza
 
