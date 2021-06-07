95efd0d580af737e1d6317244f08e0778344c33c
Helena Santos <helenafsantos8@gmail.com>
2021-06-07 18:21:00 -0300

    one hot encoding

--- scripts/Exercicio_aula9.R
+++ scripts/Exercicio_aula9.R
@@ -6,3 +6,35 @@ reciclagem <- c(1,2,3,4,4,3,3,2,1)
 recode <- c(papel = 1, plastico = 2, vidro = 3, metal = 4)
 
 (reciclagem <- factor(reciclagem, levels = recode, labels = names(recode)))
+
+#Mais fatores 
+
+install.packages("ade4")
+install.packages("arules")
+
+library(ade4)
+library(arules)
+library(poliscidata)
+library(tidyverse)
+
+
+?nes #remete a explicação do banco nes (help) 
+
+str(nes) #estrutura do banco nes 
+
+banco <- nes
+str(banco)
+
+
+# filtro por tipo de dado
+
+factorsBanco <- unlist(lapply(banco, is.factor)) 
+
+factorsBanco <- banco[ , factorsBanco]
+str(factorsBanco)
+
+# One Hot Encoding 
+
+bancoDummy <- acm.disjonctif (factorsBanco)     
+
+
