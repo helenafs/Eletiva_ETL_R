6ac61b75c2a6295475727d9240c3d17120672a26
Helena Santos <helenafsantos8@gmail.com>
2021-06-17 17:57:19 -0300

    finalização

--- scripts/Exercicio_aula11.R
+++ scripts/Exercicio_aula11.R
@@ -17,24 +17,17 @@ data("state", package = "datasets") # baixa os dados state (state.abb, state.are
 state.x77 <- as.data.frame(state.x77)  #tranforma o banco state.x77 em dataframe 
 
 
-glimpse(state.x77) #olhada nos bancos
+glimpse(state.x77) #olhada no bancos
 
 state <- toupper(state.name) # vetor dos nomes dos estados em maíuscula 
-state.x77 <- cbind(state.x77,state.abb,state.area,state) # junta os vetores ao banco state.x77 
+state.x77 <- cbind(state.x77,state) # junta os vetores ao banco state.x77 
 
 install.packages("fuzzyjoin")
 library(fuzzyjoin)
 
 # Advanced
+
 states_novo <- fuzzyjoin::stringdist_join(state.x77, states, mode='left')
-
-# Joining by: "state"
-# Erro: Names must be unique.
-# x These names are duplicated:
-#   * "state.abb" at locations 9 and 12.
-# * "state.area" at locations 10 and 13.
-# Run `rlang::last_error()` to see where the error occurred.
-
 
 baseC <- fuzzyjoin::distance_join(state.x77, states, mode='left')
 
