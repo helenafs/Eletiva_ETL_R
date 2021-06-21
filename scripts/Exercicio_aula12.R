dfe28c6cdd14732df09e2f53e37e9dc0b941f883
Helena Santos <helenafsantos8@gmail.com>
2021-06-21 18:20:53 -0300

    datas na prática - dados de Covid19 em Pernambuco

--- scripts/Exercicio_aula12.R
+++ scripts/Exercicio_aula12.R
@@ -53,3 +53,72 @@ now() %within% meuIntervalo  # investiga se está dentro do intervalo
 table( (minhaSequencia + years(1) ) %within% meuIntervalo ) # observa se a frequência de casos da sequência 1 ano na frente que estão dentro do intervalo
 
 
+# Datas na Prática - Covid em Pernambuco 
+
+url = 'https://raw.githubusercontent.com/wcota/covid19br/master/cases-brazil-states.csv' # passar a url para um objeto
+
+covidBR = read.csv2(url, encoding='latin1', sep = ',') # baixar a base de covidBR
+
+covidPE <- subset(covidBR, state == 'PE') # filtrar para Pernambuco
+
+str(covidPE) # observar as classes dos dados
+
+covidPE$date <- as.Date(covidPE$date, format = "%Y-%m-%d") # modificar a coluna data de string para date
+
+str(covidPE) # observar a mudança na classe
+
+covidPE$dia <- seq(1:length(covidPE$date)) # criar um sequencial de dias de acordo com o total de datas para a predição
+
+predDia = data.frame(dia = covidPE$dia) # criar vetor auxiliar de predição
+
+predSeq = data.frame(dia = seq(max(covidPE$dia)+1, max(covidPE$dia)+180)) # criar segundo vetor auxiliar 
+
+predDia <- rbind(predDia, predSeq) # juntar os dois 
+
+install.packages("drc")
+library(drc) # pacote para predição
+
+fitLL <- drm(deaths ~ dia, fct = LL2.5(),
+             data = covidPE, robust = 'mean') # fazendo a predição log-log com a função drm
+
+plot(fitLL, log="", main = "Log logistic") # observando o ajuste
+
+predLL <- data.frame(predicao = ceiling(predict(fitLL, predDia))) # usando o modelo para prever para frente, com base no vetor predDia
+predLL$data <- seq.Date(as.Date('2020-03-12'), by = 'day', length.out = length(predDia$dia)) # criando uma sequência de datas para corresponder aos dias extras na base de predição
+
+predLL <- merge(predLL, covidPE, by.x ='data', by.y = 'date', all.x = T) # juntando as informações observadas da base original 
+
+library(plotly) # biblioteca para visualização interativa de dados
+
+plot_ly(predLL) %>% add_trace(x = ~data, y = ~predicao, type = 'scatter', mode = 'lines', name = "Mortes - Predição") %>% add_trace(x = ~data, y = ~deaths, name = "Mortes - Observadas", mode = 'lines') %>% layout(
+  title = 'Predição de Mortes de COVID 19 em Pernambuco', 
+  xaxis = list(title = 'Data', showgrid = FALSE), 
+  yaxis = list(title = 'Mortes Acumuladas por Dia', showgrid = FALSE),
+  hovermode = "compare") # plotando tudo junto, para comparação
+
+library(zoo) # biblioteca para manipulação de datas e séries temporais
+
+covidPE <- covidPE %>% mutate(newdeathsMM7 = round(rollmean(x = deaths, 7, align = "right", fill = NA), 2)) # média móvel de 7 dias
+
+covidPE <- covidPE %>% mutate(deathsL7 = dplyr::lag(deaths, 7)) # valor defasado em 7 dias
+
+plot_ly(covidPE) %>% add_trace(x = ~date, y = ~deaths, type = 'scatter', mode = 'lines', name = "Novas Mortes") %>% add_trace(x = ~date, y = ~newdeathsMM7, name = "Novas Mortes MM7", mode = 'lines') %>% layout(
+  title = 'Novas Mortes de COVID19 em Pernambuco', 
+  xaxis = list(title = 'Data', showgrid = FALSE), 
+  yaxis = list(title = 'Novas Mortes por Dia', showgrid = FALSE),
+  hovermode = "compare") # plotando tudo junto, para comparação
+
+install.packages("xts")
+library(xts)
+
+(covidPETS <- xts(covidPE$deaths, covidPE$date)) # transformar em Série Temporal
+
+str(covidPETS) 
+
+autoplot(covidPETS) # fazer um plot apropriado para o tipo de dado
+
+acf(covidPETS) # a função acf calcula (e por padrão plota) estimativas da autocovariância ou função de autocorrelação
+
+
+
+
